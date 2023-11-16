f7f90c30f11f4c7106de8c858841167e2008de46
lyc <425572182@qq.com>
2023-11-16 15:18:30 +0800

    完成修改一级分类功能开发

--- src/main/java/com/zjgsu/crazyshopping/controller/SortController.java
+++ src/main/java/com/zjgsu/crazyshopping/controller/SortController.java
@@ -5,6 +5,7 @@ import com.zjgsu.crazyshopping.entity.SortOne;
 import com.zjgsu.crazyshopping.service.SortService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,15 @@ public class SortController {
         else {
             return RespBean.error("添加一级分类失败");
         }
+    }
+    @PutMapping("update")
+    public RespBean modifySortOne(String oldOne , String newOne){
+        if(sortService.modifySortOne(oldOne,newOne)==1){
+            return RespBean.ok("修改一级分类成功");
+        }
+        else {
+            return RespBean.error("修改一级分类失败");
+        }
+
     }
 }
--- src/main/java/com/zjgsu/crazyshopping/service/SortService.java
+++ src/main/java/com/zjgsu/crazyshopping/service/SortService.java
@@ -1,5 +1,6 @@
 package com.zjgsu.crazyshopping.service;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.zjgsu.crazyshopping.entity.RespOrderBean;
 import com.zjgsu.crazyshopping.entity.SortOne;
 import com.zjgsu.crazyshopping.mapper.SortOneMapper;
@@ -19,6 +20,13 @@ public class SortService {
     public int addSortOne(SortOne sortOne){
 
         return sortOneMapper.insert(sortOne);
+
+    }
+    public int modifySortOne(String oldOne , String newOne){
+        UpdateWrapper<SortOne> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("one",oldOne);
+        SortOne sortOne = new SortOne(newOne);
+        return sortOneMapper.update(sortOne,updateWrapper);
 
     }
 
