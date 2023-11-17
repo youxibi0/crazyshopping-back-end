7953d529ac69eb8bc8a15f94852d3e4eaa72be55
youxibi <2996750281@qq.com>
2023-11-17 12:03:30 +0800

    修改了sql文件,创建新的数据库

--- src/main/resources/data/create.sql
+++ src/main/resources/data/create.sql
@@ -1,42 +1,74 @@
+DROP TABLE IF EXISTS "account";
+CREATE TABLE "account" (
+                           "username" TEXT NOT NULL,
+                           "password" TEXT NOT NULL,
+                           "level" integer NOT NULL,
+                           PRIMARY KEY ("username")
+);
 
+INSERT INTO "account" VALUES ('admin', '123456', 0);
 
--- ----------------------------
--- Table structure for goods
--- ----------------------------
 DROP TABLE IF EXISTS "goods";
 CREATE TABLE "goods" (
-  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "name" text NOT NULL,
-  "info" TEXT,
-  "img" TEXT,
-  "price" real NOT NULL,
-  "isFreeze" integer NOT NULL DEFAULT 0,
-  "onEnable" integer NOT NULL DEFAULT 1
+                         "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                         "name" text NOT NULL,
+                         "info" TEXT,
+                         "price" real NOT NULL,
+                         "onEnable" integer NOT NULL DEFAULT 1,
+                         "num" INTEGER NOT NULL
 );
 
--- ----------------------------
--- Table structure for orders
--- ----------------------------
+
+DROP TABLE IF EXISTS "goodsImages";
+CREATE TABLE "goodsImages" (
+                               "goodsId" INTEGER NOT NULL,
+                               "imgName" TEXT NOT NULL
+);
+
+
 DROP TABLE IF EXISTS "orders";
 CREATE TABLE "orders" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "name" TEXT NOT NULL,
-  "phone" TEXT NOT NULL,
-  "time" text NOT NULL,
-  "goodsId" INTEGER NOT NULL,
-  "info" TEXT DEFAULT 等待处理,
-  "isComplete" integer NOT NULL DEFAULT 0
+                          "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                          "username" TEXT NOT NULL,
+                          "phone" TEXT NOT NULL,
+                          "location" text NOT NULL,
+                          "goodsId" INTEGER NOT NULL,
+                          "goodsInfo" TEXT,
+                          "state" integer NOT NULL DEFAULT 1,
+                          "goodsPrice" real NOT NULL,
+                          "goodsName" TEXT NOT NULL,
+                          "imgName" TEXT,
+                          "time" text NOT NULL
 );
 
 
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS "user";
-CREATE TABLE "user" (
-  "password" text NOT NULL
+DROP TABLE IF EXISTS "sortGoods";
+CREATE TABLE "sortGoods" (
+                             "one" TEXT NOT NULL,
+                             "two" TEXT NOT NULL,
+                             "goodsId" integer NOT NULL
 );
-INSERT INTO "user" VALUES ('123456');
+
+
+DROP TABLE IF EXISTS "sortOne";
+CREATE TABLE "sortOne" (
+    "one" TEXT NOT NULL
+);
+
+
+DROP TABLE IF EXISTS "sortTwo";
+CREATE TABLE "sortTwo" (
+                           "one" TEXT NOT NULL,
+                           "two" TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS "userInfo";
+CREATE TABLE "userInfo" (
+                            "username" TEXT NOT NULL,
+                            "phone" TEXT NOT NULL,
+                            "location" TEXT NOT NULL,
+                            PRIMARY KEY ("username")
+);
+
 
 
