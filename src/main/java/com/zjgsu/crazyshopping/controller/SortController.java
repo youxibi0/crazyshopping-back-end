ff4f1923bff650ef2d526e4fd8c18a3a1d6a8e8c
lyc <425572182@qq.com>
2023-11-21 19:48:03 +0800

    修改分类查询功能

--- src/main/java/com/zjgsu/crazyshopping/controller/SortController.java
+++ src/main/java/com/zjgsu/crazyshopping/controller/SortController.java
@@ -7,6 +7,7 @@ import com.zjgsu.crazyshopping.service.SortService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,13 +16,23 @@ public class SortController {
     @Autowired
     private SortService sortService;
     @RequestMapping("")
-    public List<SortOne> getAllOneSort(){
-        return sortService.getAllSortOne();
+    public List<String> getAllOneSort(){
+        List<SortOne> sortOneList = sortService.getAllSortOne();
+        List<String> result = new ArrayList<>();
+        for(SortOne sortOne : sortOneList){
+            result.add(sortOne.getOne());
+
+        }
+        return result;
     }
     @RequestMapping("/{one}")//根据一级分类名字查询二级分类
-    public List<SortTwo> getSortTwo(@PathVariable String one){
-
-        return sortService.getAllSortTwo(one);
+    public List<String> getSortTwo(@PathVariable String one){
+        List<SortTwo> sortTwoList = sortService.getAllSortTwo(one);
+        List<String> result = new ArrayList<>();
+        for(SortTwo sortTwo : sortTwoList){
+            result.add(sortTwo.getTwo());
+        }
+        return result;
     }
     @PostMapping("/add")
     public RespBean addSortOne(SortOne sortOne){
@@ -61,7 +72,6 @@ public class SortController {
     public RespBean modifySortTwo(String one ,String oldTwo ,String newTwo){
         if(sortService.modifySortTwo(one,oldTwo,newTwo)==1){
             return RespBean.ok("修改二级分类成功");
-
         }
         else {
             return RespBean.error("修改二级分类失败");
