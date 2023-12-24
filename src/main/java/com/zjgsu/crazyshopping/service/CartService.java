03d29cadb04a9d625155e4eab3cc202ecab2ced9
lyc <425572182@qq.com>
2023-12-24 19:33:20 +0800

    修改了获取购物车商品接口，使其可以获取商品的一二级分类。

--- src/main/java/com/zjgsu/crazyshopping/service/CartService.java
+++ src/main/java/com/zjgsu/crazyshopping/service/CartService.java
@@ -2,8 +2,10 @@ package com.zjgsu.crazyshopping.service;
 
 import com.zjgsu.crazyshopping.entity.Cart;
 import com.zjgsu.crazyshopping.entity.Goods;
+import com.zjgsu.crazyshopping.entity.SortGoods;
 import com.zjgsu.crazyshopping.mapper.CartMapper;
 import com.zjgsu.crazyshopping.mapper.GoodsMapper;
+import com.zjgsu.crazyshopping.mapper.SortGoodsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +20,12 @@ public class CartService {
     private CartMapper cartMapper;
     @Autowired
     private GoodsMapper goodsMapper;
+    @Autowired
+    private SortGoodsMapper sortGoodsMapper;
     public int addCart(Cart cart){
         Map<String,Object> map = new HashMap<>();
         map.put("username",cart.getUsername());
         map.put("goodsId",cart.getGoodsId());
-        System.out.println(cartMapper.selectByMap(map));
         if(!cartMapper.selectByMap(map).isEmpty()){
             return 2;
         }
@@ -35,7 +38,15 @@ public class CartService {
         cartList = cartMapper.selectByMap(map);
         List<Goods> goodsList = new ArrayList<>();
         for(Cart cart:cartList){
-            goodsList.add(goodsMapper.selectById(cart.getGoodsId()));
+            Map<String,Object> map2 = new HashMap<>();
+            map2.put("goodsId",cart.getGoodsId());
+            List<SortGoods> sortGoodsList  = sortGoodsMapper.selectByMap(map2);
+            SortGoods sortGoods = sortGoodsList.get(0);
+            Goods goods1 = goodsMapper.selectById(cart.getGoodsId());
+            Goods goods2 = new Goods(goods1.getId(),goods1.getName(),goods1.getInfo(),goods1.getPrice(),goods1.getOnEnable(),goods1.getNum(),goods1.getImgFiles());
+            goods2.setOne(sortGoods.getOne());
+            goods2.setTwo(sortGoods.getTwo());
+            goodsList.add(goods2);
         }
 
         return goodsList;
