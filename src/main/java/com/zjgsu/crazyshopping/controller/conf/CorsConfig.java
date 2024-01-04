3af75dc81f458ea6dd36a0b8e8039885294e2efa
youxibi <2996750281@qq.com>
2024-01-04 10:12:15 +0800

    基线维护:修改跨域前端地址为localhost:80

--- src/main/java/com/zjgsu/crazyshopping/controller/conf/CorsConfig.java
+++ src/main/java/com/zjgsu/crazyshopping/controller/conf/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOrigin("http://localhost:5173/");
+        corsConfiguration.addAllowedOrigin("http://localhost:80/");
         corsConfiguration.setMaxAge(3600L);
         return corsConfiguration;
     }
